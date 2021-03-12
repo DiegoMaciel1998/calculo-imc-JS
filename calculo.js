9f9a8750eb671a9dfd9f146b77f2e0eec47f4d0c
DiegoMaciel1998 <diego.maciel2019@outlook.com>
2021-03-12 20:52:01 -0300

    add the calculo.js file

--- calculo.js
+++ calculo.js
@@ -1,26 +1,32 @@
-function calcular(){
+document.getElementById('btn').addEventListener('click', () => {
+    
     let nome = document.getElementById('txt_nome').value
     let altura = parseFloat(document.getElementById('txt_altura').value)
     let peso = parseFloat(document.getElementById('txt_peso').value)
     let res = document.getElementById('res')
-    let imc = peso/(altura*altura)
+    let imc = peso / (altura * altura)
     let classificacao
-    
-        if(imc <= 18.5 ){
-            classificacao = 'Magreza'
-        
-        } else if(imc >= 18.5 && imc <= 24.8){
-            classificacao = 'Normal'
-        
-        } else if(imc >=25.0 && imc <=29.9){
-            classificacao = 'Sobrepeso'
-        
-        } else if(imc >=30.0 && imc <=39.9){
-            classificacao = 'Obesidade'
-            
-        } else{
-            classificacao = 'Obesidade grave'
-        }     
-        
-        res.innerHTML = `Olá ${nome}, o seu IMC é: ${imc.toFixed(2)} e sua classificação é: ${classificacao}`
-}
+
+    if (imc <= 18.5) {
+        classificacao = 'Magreza'
+
+    } else if (imc >= 18.5 && imc <= 24.8) {
+        classificacao = 'Normal'
+
+    } else if (imc >= 25.0 && imc <= 29.9) {
+        classificacao = 'Sobrepeso'
+
+    } else if (imc >= 30.0 && imc <= 39.9) {
+        classificacao = 'Obesidade'
+
+    } else {
+        classificacao = 'Obesidade grave'
+    }
+
+    res.innerHTML = `Olá ${nome}, o seu IMC é: ${imc.toFixed(2)} e sua classificação é: ${classificacao}`
+
+})
+
+
+
+
